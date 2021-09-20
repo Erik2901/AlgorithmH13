153e1435bdaa5937c1a7811d7c712e0590cbccbc
Erik2901 <erik2901@mail.ru>
2021-09-21 02:43:16 +0400

    hw13

--- HomeWork13.cpp
+++ HomeWork13.cpp
@@ -94,28 +94,54 @@ void check(Stack* stack,int st) {
     }
 }
 
-int checkhead(int st) {
-    int p = 0;
-    for (int r = 0; r < n; r++)
+int arr4[n];
+void depthTravers1(int st) {
+    int r;
+    visited[st] = 1;
+    for (r = 0; r < n; r++)
     {
-        if (matrix[st][r] == 1)
+        if (matrix[st][r] == 1 && !visited[r])
         {
-            p++;
+            arr4[r]++;
+            depthTravers1(r);
+        }
+        else if(matrix[st][r] == 1){
+            arr4[r]++;
         }
     }
-    return p;
 }
 
-
 int arr[n];
+int arr2[n];
 void linkc() {
     for (size_t j = 0; j < n; j++)
     {
         for (size_t i = 0; i < n; i++)
         {
-            if (matrix[j][i] == 1)
+            if (matrix[j][i] == 1 && i!=j)
             {
                 arr[i] += 1;
+            }
+        }
+    }
+    for (size_t i = 0; i < n; i++)
+    {
+        arr2[i] = i;
+    }
+    int temp;
+    int temp1;
+    for (size_t j = 0; j < n; j++)
+    {
+        for (size_t i = 0; i < n - 1; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                temp1 = arr2[i];
+                arr2[i] = arr2[i + 1];
+                arr2[i + 1] = temp;
             }
         }
     }
@@ -132,15 +158,24 @@ int main()
     std::cout << std::endl;
     ////////////
 
-    //////exercise 2
+    //////exercise 21
 
-    std::cout << checkhead(1) << std::endl;
+    depthTravers1(1);
 
+    for (size_t i = 0; i < n; i++)
+    {
+        std::cout << i << ":" << arr4[i] << "," << " ";
+    }
+    std::cout << std::endl;
+    resetArr();
+    //////////
+    
+    //// exercise 2.2
     linkc();
 
     for (size_t i = 0; i < n; i++)
     {
-        std::cout << i << ":" << arr[i] << "," << " ";
+        std::cout << arr2[i] << ":" << arr[i] << "," << " ";
     }
     std::cout << std::endl;
 
